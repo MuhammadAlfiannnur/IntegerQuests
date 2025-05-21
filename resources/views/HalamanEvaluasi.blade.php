54acd9496ecb209de9d50e52e6ce12fa06250117
MuhammadAlfiannur <alfitaki021@gmail.com>
2025-05-21 11:56:04 +0800

    perbaikan link css

--- resources/views/HalamanEvaluasi.blade.php
+++ resources/views/HalamanEvaluasi.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evaluasi Bilangan Bulat</title>
-    <link rel="stylesheet" href="{{ asset('css/StyleEvaluasi.css') }}">
+    <link rel="stylesheet" href="{{ asset('CSS/StyleEvaluasi.css') }}">
 </head>
 <body>
     <div class="container">
--- resources/views/HalamanGuru.blade.php
+++ resources/views/HalamanGuru.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Halaman Guru</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <link href="{{ asset('css/stylesHalamanGuru.css') }}" rel="stylesheet" />
+    <link href="{{ asset('CSS/stylesHalamanGuru.css') }}" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .content-section {
