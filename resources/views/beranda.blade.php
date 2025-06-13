91af8fc2bd3c8f929918ba2ea848502c63688bfd
MuhammadAlfiannur <alfitaki021@gmail.com>
2025-06-13 08:10:02 +0800

    update link COnstruct 2

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -16,7 +16,7 @@
     <div class="image_row">
         <img src="{{ asset('Gambar/cp.png') }}" alt="Capaian Pelajaran" id="CP">
         
-        <a href="https://storied-biscuit-f3350f.netlify.app/" id="Mulai">
+        <a href="https://capable-bublanina-c70ebd.netlify.app/" id="Mulai">
             <img src="{{ asset('Gambar/mulai.png') }}" alt="Memulai Media" class="linkable" >
         </a>
         {{-- <img src="{{ asset('Gambar/mulai.png') }}" alt="Memulai Media" id="Mulai"> --}}
